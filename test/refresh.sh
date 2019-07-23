5410c211b949ee1813c60267902a4fa535d3f4b8
Criveti Mihai <crivetimihai@gmail.com>
2019-07-23 06:41:48 -0500

    Refactoring

--- test/refresh.sh
+++ test/refresh.sh
@@ -3,8 +3,9 @@
 
 # Copy ID
 for host in centos7 rhel8 fedora30 ubuntu1804 debian10; do
-  (cd ${host} && vagrant reload)
-  sshpass -f password.txt ssh-copy-id vagreant@${host}
+  (cd ${host} && vagrant destroy -f)
+  (cd ${host} && vagrant up)
+  sshpass -f password.txt ssh-copy-id vagrant@${host}
 done
 
 
